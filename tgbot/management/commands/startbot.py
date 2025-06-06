74884929401287d79f2ef63f77f2b4910ae5c845
akent4000 <akent4000@yandex.ru>
2025-06-06 13:32:39 +0300

    f

--- tgbot/management/commands/startbot.py
+++ tgbot/management/commands/startbot.py
@@ -23,7 +23,7 @@ _test_thread = None
 
 def _run_main_bot():
     """Цикл polling для основного бота"""
-    from tgbot.handlers import commands, main_menu, moon_calc, apod, utils, int_facts
+    from tgbot.handlers import commands, main_menu, moon_calc, apod, utils, int_facts, articles
 
     while True:
         try:
