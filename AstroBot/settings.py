1710d9bbf87a1d90c87ae9c480e192c43672fbdf
akent4000 <akent4000@yandex.ru>
2025-06-07 20:08:26 +0300

    f

--- AstroBot/settings.py
+++ AstroBot/settings.py
@@ -49,18 +49,17 @@ STATIC_ROOT = '/www/static/'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
 
     'tgbot',
     'rangefilter',
     'solo',
     'nested_admin',
-    'django.contrib.admin',
     'jazzmin',
 ]
 
