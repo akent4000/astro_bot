888ddf5ef64bf85af08c0a071608ad8839e9fd72
akent4000 <akent4000@yandex.ru>
2025-06-07 20:39:39 +0300

    f

--- tgbot/urls.py
+++ tgbot/urls.py
@@ -1,11 +1,8 @@
 from django.views.generic import RedirectView
 from AstroBot import settings
-from django.urls import include, path
-from django.contrib import admin
+from django.urls import path
 app_name = 'tgbot'
 
 urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
-    path('admin_tools/', include('admin_tools.urls')),
-    path('admin/', admin.site.urls),
 ]
