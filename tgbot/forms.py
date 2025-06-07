fb753873b15e1cd0da623ae24aad7201d671a98d
akent4000 <akent4000@yandex.ru>
2025-06-07 22:00:41 +0300

    test

--- tgbot/forms.py
+++ tgbot/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from tgbot.models import SSHKey, TelegramUser
+from django.forms import inlineformset_factory
+from tgbot.models import SSHKey, TelegramUser, Quiz, Question, Choice
 
 class SSHKeyAdminForm(forms.ModelForm):
     # Дополнительные поля используются только при создании ключа
@@ -36,3 +37,47 @@ class SendMessageForm(forms.Form):
         widget=forms.Select,
         required=True
     )
+
+class QuizForm(forms.ModelForm):
+    class Meta:
+        model = Quiz
+        fields = ['title', 'topic', 'level']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'topic': forms.Select(attrs={'class': 'form-control'}),
+            'level': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['text', 'explanation', 'order']
+        widgets = {
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
+            'explanation': forms.Textarea(attrs={'class': 'form-control', 'rows': 1}),
+            'order': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
+        }
+
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['text', 'is_correct', 'order']
+        widgets = {
+            'text': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_correct': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'order': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
+        }
+
+# Inline formsets
+QuestionFormSet = inlineformset_factory(
+    Quiz, Question,
+    form=QuestionForm,
+    extra=1,
+    can_delete=True
+)
+ChoiceFormSet = inlineformset_factory(
+    Question, Choice,
+    form=ChoiceForm,
+    extra=2,
+    can_delete=True
+)
--- tgbot/urls.py
+++ tgbot/urls.py
@@ -1,8 +1,12 @@
 from django.views.generic import RedirectView
 from AstroBot import settings
 from django.urls import path
+from .views import quiz_editor
+
 app_name = 'tgbot'
 
 urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
+    path('editor/quiz/new/', quiz_editor, name='quiz_create'),
+    path('editor/quiz/<int:pk>/edit/', quiz_editor, name='quiz_edit'),
 ]
